6c58c34780a8a87ed4fa0e81a119d990ccef38ca
eduardoMKR <eduardo.kusaba.rosa@escola.pr.gov.br>
2026-06-18 12:11:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 let fontSize = 18;
 
+function applyFont() {
+  document.body.style.fontSize = fontSize + "px";
+}
+
 function increaseFont() {
   fontSize += 2;
-  document.body.style.fontSize = fontSize + "px";
+  applyFont();
 }
 
 function decreaseFont() {
   fontSize = Math.max(12, fontSize - 2);
-  document.body.style.fontSize = fontSize + "px";
+  applyFont();
 }
 
 function toggleContrast() {
   document.body.classList.toggle("high-contrast");
+}
+
+function toggleReadable() {
+  document.body.classList.toggle("readable");
 }
